6fefa77c13b5e01ffe325c127a8170d3ae3a0cd9
osaroadade <adadechuks@gmail.com>
2021-07-27 01:06:47 +0100

    How ever was snow white able to dress the dwarfs?

--- docs/index.js
+++ docs/index.js
@@ -14,6 +14,12 @@ const cancelSearch = document.querySelector(".cancel-search")
 const savedLists = localStorage.getItem("tasks")
 const showCompleted = document.querySelector(".completed-btn")
 const showAll = document.querySelector(".show-all")
+const deleteBtn = document.getElementsByClassName("delete-btn")
+const editItemBtn = document.getElementsByClassName("edit-item-btn")
+const editBtn = document.querySelector(".edit-btn")
+const doneBtn = document.querySelector(".done-btn")
+const deleteBtnItems = document.getElementsByClassName("delete-btn-items")
+const editBtnItems = document.getElementsByClassName("edit-btn-items")
 
 let taskItems = []
 let parsedSavedLists = JSON.parse(savedLists)
@@ -24,8 +30,12 @@ function renderAll() {
         for (let i = 0; i < taskItems.length; i++) {
             containerItems += `
                 <div class="reminder-item">
+                    <button class="action-btn edit-btn-items d-none">Edit</button>
+                    <button class="delete-btn d-none"><img src="./assets/delete.svg"></button>
                     <div><span class="circle"></span></div>
                     <p class="list-item">${taskItems[i].name}</p>
+                    <button class="edit-item-btn d-none"><img src="./assets/edit.svg"></button>
+                    <button class="action-btn delete-btn-items d-none">Delete</button>
                 </div>
             `
         }
@@ -156,14 +166,16 @@ for (let i = 0; i < addReminder.length; i++) {
     })
 }
 
-//Render Reminder Status Type
+//Toggle buttons on click in title continers
+function actionWithClass(name1, name2) {
+    name1.classList.toggle("d-none")
+    name2.classList.toggle("d-none")
+}
+
+//Render Reminder Status Type (Show all and show completed)
 for (let i = 0; i < listItem.length; i++) {
-    function actionWithClass(action1, action2) {
-        showCompleted.classList[action1]("d-none")
-        showAll.classList[action2]("d-none")
-    }
     showCompleted.addEventListener("click", () => {
-        actionWithClass("add", "remove")
+        actionWithClass(showCompleted, showAll)
         if (listItem[i].classList.contains("todo-done")) {
             emptyState.classList.remove("d-block")
         } else {
@@ -173,10 +185,57 @@ for (let i = 0; i < listItem.length; i++) {
         }
     })
     showAll.addEventListener("click", () => {
-        actionWithClass("remove", "add")
+        actionWithClass(showCompleted, showAll)
         if (reminderItem[i].classList.contains("d-none")) {
             reminderItem[i].classList.remove("d-none")
             emptyState.classList.remove("d-block")
+        }
+    })
+}
+
+// Render Edit Actions for reminder items
+function renderEditActions(elementName) {
+    function toggleEditActions(action, className) {
+        for (const i of action) {
+            i.classList.toggle(className)
+        }
+    }
+    elementName.addEventListener("click", () => {
+        actionWithClass(editBtn, doneBtn)
+        toggleEditActions(deleteBtn, "d-none")
+        toggleEditActions(editItemBtn, "d-none")
+        toggleEditActions(reminderItem, "editable")
+        reminderList.classList.toggle("reminder-list-editable")
+
+        if (elementName === doneBtn) {
+            for (const i of editBtnItems) {
+                if (!i.classList.contains("d-none")) {
+                    i.classList.toggle("d-none")
+                }
+            }
+            for (const i of deleteBtnItems) {
+                if (!i.classList.contains("d-none")) {
+                    i.classList.toggle("d-none")
+                }
+            }
+        }
+    })
+}
+
+renderEditActions(editBtn)
+renderEditActions(doneBtn)
+
+for (let i = 0; i < reminderItem.length; i++) {
+    editItemBtn[i].addEventListener("click", () => {
+        editBtnItems[i].classList.toggle("d-none")
+        if (!deleteBtnItems[i].classList.contains("d-none")) {
+            deleteBtnItems[i].classList.toggle("d-none")
+        }
+    })
+    deleteBtn[i].addEventListener("click", () => {
+        deleteBtnItems[i].classList.toggle("d-none")
+        if (!editBtnItems[i].classList.contains("d-none")) {
+            editBtnItems[i].classList.toggle("d-none")
         }
     })
 }
